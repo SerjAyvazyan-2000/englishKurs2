f4b9e6292cf0e894de47b78b70c6f61badca0532
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-04-12 00:15:34 +0300

    new

---
+++ assets/js/index.js
@@ -0,0 +1,75 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const header = document.querySelector('header');
+const menuBg = document.querySelector('.menu-bg');
+const headerBody = document.querySelector('.header-body');
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    burger.addEventListener("click", function (e) {
+        if (menu.classList.contains('active')) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    });
+    menuBg.addEventListener("click", function (e) {
+        if (menu.classList.contains('active')) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    });
+
+    function openMenu() {
+        menu.classList.add('active');
+        burger.classList.add('active');
+        header.classList.add('active');
+        menuBg.classList.add('active');
+        headerBody.classList.add('active');
+
+
+    }
+
+    function closeMenu() {
+        menu.classList.remove('active');
+        burger.classList.remove('active');
+        header.classList.remove('active');
+        menuBg.classList.remove('active');
+        headerBody.classList.remove('active');
+
+    }
+
+
+});
+
+window.addEventListener('scroll', function () {
+    const headerTop = document.querySelector('header')
+
+
+    if (window.scrollY > 0) {
+        headerTop.classList.add('moved');
+
+    } else {
+        headerTop.classList.remove('moved');
+
+    }
+});
+
+
+const questions = document.querySelectorAll('.faq-question');
+
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+        question.classList.toggle('active');
+        const answer = question.nextElementSibling;
+
+        if (question.classList.contains('active')) {
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        } else {
+            answer.style.maxHeight = null;
+        }
+    });
+});
+
